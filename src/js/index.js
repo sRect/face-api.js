9110946fe54f9eb8df020e9ecf531d98e3328774
fangchaoqun <fcqun@sina.cn>
2020-01-03 16:14:30 +0800

    feat: :sparkles: 人脸识别、表情识别、年龄性别识别、头像提取

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import * as faceapi from "face-api.js";
-import ikJPG from '@/assets/img/ik.jpg';
+// import ikJPG from '@/assets/img/ik.jpg';
 
 console.log(faceapi.nets);
 
@@ -8,7 +8,6 @@ class ShowImage {
 
     this.file = null;
     this.imgWrap = document.querySelector("#imgWrap");
-    this.base64 = null;
   }
 
   get inputFile() {
@@ -16,11 +15,7 @@ class ShowImage {
   }
 
   get previewImg() {
-    return document.querySelector("#previewImg");
-  }
-
-  get imgBase64() {
-    return this.base64;
+    return document.getElementById("previewImg");
   }
 
   previewImage(file) { // 选择本地图片并预览
@@ -30,7 +25,6 @@ class ShowImage {
     fileReader.readAsDataURL(fileBlob);
     fileReader.onload = e => {
       const path = e.target.result;
-      this.base64 = path;
       previewImg.src = path;
       previewImg.className = 'img-responsive';
       previewImg.style.width = '100%';
@@ -46,6 +40,10 @@ class ShowImage {
     let file = target.files[0];
     this.file = file;
     this.previewImage(file);
+
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
   bindEvents() {
@@ -67,9 +65,11 @@ class Face extends ShowImage {
     this.file = null;
     this.btn = document.querySelector("#btn");
     this.canvas = document.querySelector("#canvas");
+    this.faceExtractionBtn = document.querySelector("#faceExtractionBtn");
+    this.faceExtractWrap = document.querySelector("#faceExtractWrap");
   }
 
-  async loadWeight() {
+  async loadWeight() { // 加载模型
     await faceapi.nets.ssdMobilenetv1.load('/static/weights/ssd_mobilenetv1_model-weights_manifest.json');
     await faceapi.nets.faceLandmark68Net.load('/static/weights/face_landmark_68_model-weights_manifest.json');
     await faceapi.nets.faceExpressionNet.load('/static/weights/face_expression_model-weights_manifest.json');
@@ -77,32 +77,60 @@ class Face extends ShowImage {
     await faceapi.nets.ageGenderNet.load('/static/weights/age_gender_model-weights_manifest.json');
   }
 
-  async getDetections() {
+  async getDetections() { // 进行识别
     const previewImg = this.previewImg;
     if (previewImg) {
-      // all faces
+      const canvas = this.canvas;
+      const minProbability = 0.05;
+      const displaySize = { width: previewImg.width, height: previewImg.height };
       const detections = await faceapi
         .detectAllFaces(previewImg)
-        .withFaceLandmarks()
-        // .withFaceExpressions()
-        // .withAgeAndGender()
+        .withFaceLandmarks() // 人脸68特征
+        .withFaceExpressions() // 人脸表情识别
+        .withAgeAndGender() // 年龄和性别识别
         // .withFaceDescriptors()
       console.log("detections", detections);
-      const displaySize = { width: this.previewImg.width, height: this.previewImg.height };
-      // const ctx = this.canvas.getContext("2d");
-      // ctx.drawImage(this.previewImg, 0, 0);
-      // faceapi.matchDimensions(this.canvas, displaySize);
-
+      
+      faceapi.matchDimensions(canvas, displaySize);
       const resizedDetections = faceapi.resizeResults(detections, displaySize);
-      faceapi.draw.drawDetections(this.canvas, resizedDetections);
-      // const minProbability = 0.05;
-      // faceapi.draw.drawFaceExpressions(this.canvas, resizedDetections, minProbability);
-      faceapi.draw.drawFaceLandmarks(this.canvas, resizedDetections);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections, minProbability);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+
+      resizedDetections.forEach(result => {
+        console.log(result);
+        const { age, gender, genderProbability } = result
+        new faceapi.draw.DrawTextField(
+          [
+            `${faceapi.utils.round(age, 0)} years`,
+            `${gender} (${faceapi.utils.round(genderProbability)})`
+          ],
+          result.detection.box.topLeft
+        ).draw(canvas)
+      })
     }
   }
 
+  async handleFaceExtractions() { // 人脸提取
+    const previewImg = this.previewImg;
+    if (previewImg) {
+      const canvas = this.canvas;
+      const options = new faceapi.SsdMobilenetv1Options();
+      const displaySize = { width: previewImg.width, height: previewImg.height };
+      const detections = await faceapi.detectAllFaces(previewImg, options);
+      const faceImages = await faceapi.extractFaces(previewImg, detections);
+
+      faceapi.matchDimensions(canvas, displaySize);
+      this.faceExtractWrap.innerHTML = "";
+
+      faceImages.forEach(canvas => this.faceExtractWrap.appendChild(canvas));
+    }
+    
+  }
+
   bindEvents() {
-    this.btn.addEventListener("click", this.getDetections.bind(this))
+    this.btn.addEventListener("click", this.getDetections.bind(this));
+    this.faceExtractionBtn.addEventListener('click', this.handleFaceExtractions.bind(this));
   }
 
   init() {
