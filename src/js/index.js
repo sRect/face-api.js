72386512bbdb7c0c0a56922211d9cf48df8ac253
srect <fcqun@sina.cn>
2022-11-27 17:29:06 +0800

    feat: 初步完成

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,11 @@ class App {
   constructor() {
     this.closed = true;
     this.timer = null;
+    this.next = false;
+    this.curSlectIndex = 0; // 当前选择的进度
+    this.curSelectStatus = ""; // 选择的左侧还是右侧
+    this.oldSelectSrc = ""; // 选择图片的src
+
     // this.worker = new Worker();
     this.video = document.querySelector("#video");
     this.startBtn = document.querySelector("#startBtn");
@@ -22,6 +27,7 @@ class App {
   handleFaceLeftOrRight(landmarks) {
     // const landmarks = result.landmarks;
 
+    const DIFF_NUM = 15;
     let leftEye = landmarks.getLeftEye();
     let rightEye = landmarks.getRightEye();
     // let nose = landmarks.getNose();
@@ -59,7 +65,7 @@ class App {
     // console.log(leftEyeAvgPoint, rightEyeAvgPoint, noseAvgPoint);
     let diff = Math.abs(leftEyeAvgPoint.y - rightEyeAvgPoint.y);
 
-    return diff > 15
+    return diff > DIFF_NUM
       ? leftEyeAvgPoint.y > rightEyeAvgPoint.y
         ? "left"
         : "right"
@@ -77,9 +83,9 @@ class App {
     // await faceapi.nets.faceExpressionNet.load(
     //   "/static/weights/face_expression_model-weights_manifest.json"
     // );
-    // await faceapi.nets.faceRecognitionNet.load(
-    //   "/static/weights/face_recognition_model-weights_manifest.json"
-    // );
+    await faceapi.nets.faceRecognitionNet.load(
+      "/static/weights/face_recognition_model-weights_manifest.json"
+    );
     await faceapi.nets.ageGenderNet.load(
       "/static/weights/age_gender_model-weights_manifest.json"
     );
@@ -87,7 +93,7 @@ class App {
     console.log("模型加载完成");
   }
 
-  async handleVideoFaceTracking() {
+  async handleVideoFaceTracking(cb) {
     if (this.closed) {
       clearTimeout(this.timer);
       return;
@@ -103,56 +109,10 @@ class App {
     const resizedResults = faceapi.resizeResults(results, dims);
 
     // console.log("options==>", options);
-    console.log("resizedResults==>", resizedResults);
-    if (resizedResults.length) {
-      requestAnimationFrame(() => {
-        !this.warn.classList.contains("hidden") &&
-          this.warn.classList.add("hidden");
-      });
+    // console.log("resizedResults==>", resizedResults);
+    cb && cb(resizedResults);
 
-      resizedResults.forEach((result) => {
-        let { landmarks, gender, genderProbability } = result;
-        // let gender = result.gender; // male 男
-        // let genderProbability = result.genderProbability;
-        // console.log("result.detection==>", result.detection);
-
-        let resultStr = this.handleFaceLeftOrRight(landmarks);
-        console.log("resultStr==>", resultStr);
-
-        switch (resultStr) {
-          case "center":
-            this.heart.classList.remove("heartLeftActive");
-            this.heart.classList.remove("heartRightActive");
-            this.left.classList.remove("leftActive");
-            this.right.classList.remove("rightActive");
-            break;
-          case "left":
-            this.heart.classList.remove("heartLeftActive");
-            this.heart.classList.remove("heartRightActive");
-            this.left.classList.remove("leftActive");
-            this.right.classList.remove("rightActive");
-
-            this.heart.classList.add("heartRightActive");
-            this.right.classList.add("rightActive");
-            break;
-          case "right":
-            this.heart.classList.remove("heartLeftActive");
-            this.heart.classList.remove("heartRightActive");
-            this.left.classList.remove("leftActive");
-            this.right.classList.remove("rightActive");
-
-            this.heart.classList.add("heartLeftActive");
-            this.left.classList.add("leftActive");
-            break;
-          default:
-            break;
-        }
-      });
-    } else {
-      this.warn.classList.remove("hidden");
-    }
-
-    this.timer = setTimeout(() => this.handleVideoFaceTracking());
+    this.timer = setTimeout(() => this.handleVideoFaceTracking(cb));
   }
 }
 
@@ -163,6 +123,21 @@ class DyImageSelect extends App {
     // window.devicePixelRatio
     this.width = document.documentElement.clientWidth;
     this.height = document.documentElement.clientHeight;
+
+    this.imgSlectObj = {
+      male: [
+        ["/static/img/girl1.jpg", "/static/img/rmb1.jpg"],
+        ["/static/img/girl3.jpg", "/static/img/rmb2.jpg"],
+        ["/static/img/girl2.jpg", "/static/img/gold.png"],
+        ["/static/img/girl4.png", "/static/img/ayst.png"],
+      ],
+      female: [
+        ["/static/img/boy1.png", "/static/img/boy2.png"],
+        ["/static/img/boy3.png", "/static/img/boy4.png"],
+        ["/static/img/boy5.png", "/static/img/boy6.png"],
+        ["/static/img/boy7.png", "/static/img/boy8.png"],
+      ],
+    };
   }
 
   // 获取摄像头视频流
@@ -207,10 +182,15 @@ class DyImageSelect extends App {
       this.startBtn.classList.remove("loading");
       this.startBtn.classList.add("hidden");
       this.closeBtn.classList.remove("hidden");
-      this.imgSelectWrap.classList.remove("hidden");
+
       this.video.play();
 
-      await this.handleVideoFaceTracking();
+      let img1 = this.left.querySelector("img");
+      let img2 = this.right.querySelector("img");
+      img1.src = this.imgSlectObj.male[this.curSlectIndex][0];
+      img2.src = this.imgSlectObj.male[this.curSlectIndex][1];
+
+      await this.handleVideoFaceTracking(this.handleFaceData.bind(this));
     };
   }
 
@@ -218,10 +198,10 @@ class DyImageSelect extends App {
   async closeCamera() {
     this.closed = true;
     clearTimeout(this.timer);
+    this.imgSelectWrap.classList.add("hidden");
     this.startBtn.classList.remove("loading");
     this.startBtn.classList.remove("hidden");
     this.closeBtn.classList.add("hidden");
-    this.imgSelectWrap.classList.add("hidden");
     this.startBtn.removeAttribute("disabled");
     this.warn.classList.add("hidden");
     // https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/stop
@@ -232,6 +212,113 @@ class DyImageSelect extends App {
     });
 
     this.video.srcObject.srcObject = null;
+  }
+
+  handleFaceData(resizedResults) {
+    // console.log("resizedResults==>", resizedResults);
+    if (resizedResults.length) {
+      requestAnimationFrame(() => {
+        !this.warn.classList.contains("hidden") &&
+          this.warn.classList.add("hidden");
+      });
+
+      let img1 = this.left.querySelector("img");
+      let img2 = this.right.querySelector("img");
+
+      resizedResults.forEach((result) => {
+        let { landmarks, gender } = result;
+        // console.log("result", result);
+        // let gender = result.gender; // male 男 woman 女
+        // let genderProbability = result.genderProbability;
+        // console.log("result.detection==>", result.detection);
+        let sex = gender === "male" ? "male" : "female";
+        this.imgSelectWrap.classList.contains("hidden") &&
+          this.imgSelectWrap.classList.remove("hidden");
+
+        // console.log("gender==>", gender);
+
+        if (gender && this.curSlectIndex < this.imgSlectObj[sex].length) {
+          let index =
+            this.curSlectIndex + 1 >= this.imgSlectObj[sex].length
+              ? this.curSlectIndex
+              : this.curSlectIndex + 1;
+
+          if (this.curSelectStatus === "left") {
+            // console.log("this.curSlectIndex==>", this.curSlectIndex);
+
+            img1.src = this.oldSelectSrc;
+            img2.src = this.imgSlectObj[sex][index][1];
+          } else if (this.curSelectStatus === "right") {
+            // console.log("this.curSlectIndex==>", this.curSlectIndex);
+
+            img1.src = this.imgSlectObj[sex][index][0];
+            img2.src = this.oldSelectSrc;
+          }
+        } else {
+          console.log("选择完成");
+        }
+
+        let resultStr = this.handleFaceLeftOrRight(landmarks);
+        // console.log("resultStr==>", resultStr);
+
+        switch (resultStr) {
+          case "center":
+            this.curSelectStatus = "";
+            this.heart.classList.remove("heartRightActive");
+            this.heart.classList.remove("heartLeftActive");
+            this.left.classList.remove("leftActive");
+            this.right.classList.remove("rightActive");
+            this.imgSelectWrap.classList.remove("imgSelectRightActive");
+            this.imgSelectWrap.classList.remove("imgSelectLeftActive");
+            break;
+          case "left":
+            this.next = true;
+            this.curSelectStatus = "right";
+            this.oldSelectSrc = img2.getAttribute("src");
+
+            this.heart.classList.remove("heartLeftActive");
+            // this.heart.classList.remove("heartRightActive");
+            this.left.classList.remove("leftActive");
+            // this.right.classList.remove("rightActive");
+            // this.imgSelectWrap.classList.remove("imgSelectRightActive");
+            this.imgSelectWrap.classList.remove("imgSelectLeftActive");
+
+            this.heart.classList.add("heartRightActive");
+            this.right.classList.add("rightActive");
+            this.imgSelectWrap.classList.add("imgSelectRightActive");
+            break;
+          case "right":
+            this.next = true;
+            this.curSelectStatus = "left";
+            this.oldSelectSrc = img1.getAttribute("src");
+
+            // this.heart.classList.remove("heartLeftActive");
+            this.heart.classList.remove("heartRightActive");
+            // this.left.classList.remove("leftActive");
+            this.right.classList.remove("rightActive");
+            this.imgSelectWrap.classList.remove("imgSelectRightActive");
+            // this.imgSelectWrap.classList.remove("imgSelectLeftActive");
+
+            this.heart.classList.add("heartLeftActive");
+            this.left.classList.add("leftActive");
+            this.imgSelectWrap.classList.add("imgSelectLeftActive");
+            break;
+          default:
+            break;
+        }
+
+        // this.timer2 = setTimeout(() => {
+
+        // }, 800);
+        if (this.next && !this.curSelectStatus) {
+          console.log("enter==>");
+          this.next = false;
+          this.curSlectIndex++;
+        }
+      });
+    } else {
+      this.warn.classList.remove("hidden");
+    }
   }
 
   async init() {
